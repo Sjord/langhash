40f53cb383b857b3afec1ba30a3124756a588534
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2017-08-10 16:16:45 +0200

    Put password on the page.

--- src/background.js
+++ src/background.js
@@ -6,8 +6,9 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     let masterPassword = prompt('Please enter your password:');
     if (masterPassword) {
         let domain = tldjs.getDomain(tab.url);
-        pbkdf2.pbkdf2(masterPassword, domain, 100000, 32, 'sha512', (err, derivedKey) => {
-            alert(ascii85.encode(derivedKey));
+        pbkdf2.pbkdf2(masterPassword, domain, 100000, 16, 'sha512', (err, derivedKey) => {
+            let password = ascii85.encode(derivedKey).toString();
+            chrome.tabs.executeScript({"code": `document.activeElement.value=${JSON.stringify(password)};`});
         });
     }
 });
