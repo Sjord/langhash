21c0aceedd49c56456f2570bf40358c5ea047d82
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2017-09-26 17:07:03 +0200

    Make dist the extension directory
    So that we only get the necessary files in the packed extension.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+VERSION=$(node -p "require('./package.json').version")
+
 npm install
 node_modules/.bin/webpack
 cp src/*.html dist
+cat src/manifest.json | sed "s/VERSION/$VERSION/g" > dist/manifest.json
